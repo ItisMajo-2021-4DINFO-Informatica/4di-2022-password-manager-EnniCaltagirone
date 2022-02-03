92aa00520737cbcbf13d7588e6ef59b82f95989e
EnniCaltagirone <ennixenni@gmail.com>
2022-02-03 12:38:45 +0100

    file corrente

--- Notes/Notes/Notes/Models/Note.cs
+++ Notes/Notes/Notes/Models/Note.cs
@@ -4,8 +4,7 @@ namespace Notes.Models
 {
     public class Note
     {
-        [PrimaryKey, AutoIncrement]
-        public int ID { get; set; }
+        public string Filename { get; set; }
         public string ServiceName { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
